c6fc148ecf6fa18ce805cf739bc7398f85afa0d9
sadat <shameemsadat00@gmail.com>
2025-03-08 12:06:37 +0600

    first commit

---
+++ pythonBasics/conditional_statement.py
@@ -0,0 +1,78 @@
+a=20
+b=20
+
+
+'''
+
+if a>b :
+    print("a is greater")
+else:
+
+    if a<b:
+        print("b is greater")
+    else:
+        print("both are equal")
+
+
+'''
+
+
+'''
+if a>b :
+    print("a is greater")
+elif a<b:
+    print("b is greater")
+else:
+    print("both are equal")
+
+'''
+
+
+'''
+grade a student based on mark
+
+80 - 100 : A
+70 - 79 : B
+50 - 69 : C
+else : F
+'''
+
+'''
+logical operator
+and: True, when everything is true 
+or: True, when one of them is true
+
+and Table:
+false and false= false
+false and true=false
+true and false=false
+true and true = true
+
+'''
+
+number = 56
+
+if  80 <= number and number<=100:
+    print("grade A")
+
+elif 70<= number and number<80:
+    print("grade B")
+
+elif 50<=number and number<70:
+    print("grade C")
+
+else:
+    print("grade F")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pythonBasics/variables_datatype.py
@@ -0,0 +1,82 @@
+#this is a comment
+
+#print("helloooo")
+
+'''
+data type:
+
+number
+string
+list
+dictionary
+'''
+
+'''
+variables  : 
+storage, to store some value
+you can change the value later
+
+'''
+
+'''
+indentation: alignment
+'''
+
+#example 1: Number
+
+x = 5  #   =  assign 
+y = 5 + 3
+
+
+#example 2: string
+
+s = "abcdefgh"
+
+# print(s)
+
+# s = "xyz"
+
+
+#example 3: list/array 
+#a collection of data
+
+heights = [5.7, 5.5, 4.9]
+'''
+indexing: serial number
+starts from 0
+0,1,2,3,4,5.......
+to access a specifix index from a list
+use []
+for example, heights[1]
+
+fast access
+'''
+
+#example 4: dictionary or mapping
+'''
+key: value
+key: value
+'''
+
+freq = {
+    'c':1,
+    'a':3,
+    'b':2,
+    'd':1
+}
+
+'''
+to access value, use key
+for example  freq['a']
+slower than list
+'''
+
+print(freq['b'])
+
+
+
+
+
+
+
+
